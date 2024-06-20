1bdbb0fbc0904084f86d1060c8b8c130f545522e
amiraadev <allagui_fawzi@outlook.fr>
2024-06-20 15:25:26 +0100

    done with the auth.resolver by implementing these mutations : register, login, logout, refreshToken

--- backend/src/auth/auth.resolver.ts
+++ backend/src/auth/auth.resolver.ts
@@ -1,9 +1,53 @@
-import { Resolver,Query } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Context } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { LoginResponse, RegisterResponse } from './types';
+import { LoginDto, RegisterDto } from './auth.dto';
+import { Request, Response } from 'express';
+import { BadRequestException } from '@nestjs/common';
 
 @Resolver()
 export class AuthResolver {
-    @Query(()=>String) 
-    async hello() {
-        return 'hello'
+  constructor(private readonly authService: AuthService) {}
+
+  @Mutation(() => RegisterResponse)
+  async register(
+    @Args('registerInput') registerDto: RegisterDto,
+    @Context() context: { res: Response },
+  ) {
+    if (registerDto.password !== registerDto.confirmPassword) {
+      throw new BadRequestException({
+        confirmPassword: 'Password and confirm password are not the same',
+      });
     }
+    const { user } = await this.authService.register(registerDto, context.res);
+    return { user };
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Args('loginInput') loginDto: LoginDto,
+    @Context() context: { res: Response },
+  ) {
+    return this.authService.login(loginDto, context.res);
+  }
+
+  @Mutation(() => String)
+  async logout(@Context() context: { res: Response }) {
+    return this.authService.logout(context.res);
+  }
+
+  @Query(() => String)
+  async hello() {
+    return 'hello';
+  }
+
+  @Mutation(() => String)
+  async refreshToken(@Context() context: { req: Request; res: Response }) {
+    console.log('refreshToken');
+    try {
+        return this.authService.refreshToken(context.req,context.res);
+    } catch (error) {
+        throw new BadRequestException(error.message)
+    }
+  }
 }
--- backend/src/auth/types.ts
+++ backend/src/auth/types.ts
@@ -1,8 +1,14 @@
-import {Field, ObjectType} from '@nestjs/graphql'
-import { User } from 'src/user/user.type'
+import { Field, ObjectType } from '@nestjs/graphql';
+import { User } from 'src/user/user.type';
 
 @ObjectType()
 export class RegisterResponse {
-    @Field(() => User , {nullable:true})
-    user?: User
+  @Field(() => User, { nullable: true })
+  user?: User;
+}
+
+@ObjectType()
+export class LoginResponse {
+  @Field(() => User)
+  user: User;
 }
