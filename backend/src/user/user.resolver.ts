3e772651ea7cb4845a2b31b306cbf46cf92da910
amiraadev <allagui_fawzi@outlook.fr>
2024-06-20 15:56:56 +0100

    adding the type.d.ts file so that we can change the structure of the request to contain a user field

--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -1,4 +1,25 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Mutation, Args, Context } from '@nestjs/graphql';
+import { UserService } from './user.service';
+import { User } from './user.type';
+import { Request } from 'express';
+import { UseGuards } from '@nestjs/common';
+import { GraphqlAuthGuard } from 'src/auth/graphql-auth.guard';
+import { createWriteStream } from 'fs';
+import { join } from 'path';
+import { v4 as uuidv4 } from 'path';
+import * as GraphQLUpload from 'graphql-upload/GraphQLUpload.js';
 
 @Resolver()
-export class UserResolver {}
+export class UserResolver {
+
+    constructor(private readonly userService :UserService){}
+
+    UseGuards(GraphqlAuthGuard)
+    @Mutation(()=>User)
+    async updateProfile(
+        @Args('fullname') fullname: string,
+        @Args('file',{type:() => GraphQLUpload, nullable: true})
+        file:GraphQLUpload.FileUpload,
+        @Context() context:{req:Request}
+    ){}
+}
