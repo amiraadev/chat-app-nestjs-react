f8291c5c20ab24d61595174581f1f42a6250e875
amiraadev <allagui_fawzi@outlook.fr>
2024-06-20 18:24:39 +0100

    set up of the mantine-ui with the vite

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+/** @format */
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import "@mantine/core/styles.css";
+
+import { MantineProvider } from "@mantine/core";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<MantineProvider>
+			<App />
+		</MantineProvider>
+	</React.StrictMode>
+);
