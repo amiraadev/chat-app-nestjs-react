47c73724c0def2575523803925d673b151a21177
amiraadev <allagui_fawzi@outlook.fr>
2024-06-20 16:11:26 +0100

    done with the user.resolver.ts : implemented the updateProfile function , including the avatarUrl through the storeImageAndGetUrl function

--- backend/src/user/user.resolver.ts
+++ backend/src/user/user.resolver.ts
@@ -14,12 +14,26 @@ export class UserResolver {
 
     constructor(private readonly userService :UserService){}
 
-    UseGuards(GraphqlAuthGuard)
+    @UseGuards(GraphqlAuthGuard)
     @Mutation(()=>User)
     async updateProfile(
         @Args('fullname') fullname: string,
         @Args('file',{type:() => GraphQLUpload, nullable: true})
         file:GraphQLUpload.FileUpload,
-        @Context() context:{req:Request}
-    ){}
+        @Context() context:{req:Request}    
+    ){
+        const imageUrl = file ? await this.storeImageAndGetUrl(file):null;
+        const userId = context.req.user.sub;
+        return this.userService.updateProfile(userId,fullname,imageUrl)
+    }
+
+    private async storeImageAndGetUrl (file:GraphQLUpload){
+        const {createReadStream,filename} = await file;
+        const uniqueFilename = `${uuidv4()}_${filename }`;
+        const imagePath = join(process.cwd(),'public',uniqueFilename);
+        const imageUrl = `${process.env.APP_URL}/${uniqueFilename}`
+        const readStream = createReadStream();
+        readStream.pipe(createWriteStream(imagePath));
+        return imageUrl
+    }
 }
