4ca0a477727a84945fe9767cef546b9fbfaa5e64
amiraadev <allagui_fawzi@outlook.fr>
2024-06-21 18:42:34 +0100

    setting up a WebSocket connection for a GraphQL subscription : GraphQL subscriptions are a powerful feature that allows for real-time data updates between a GraphQL client (like a web app) and a server

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -28,7 +28,11 @@ const pubsub = new RedisPubSub({
       imports: [ConfigModule, AppModule],
       inject: [ConfigService],
       driver: ApolloDriver,
-      useFactory: async (configService: ConfigService) => {
+      useFactory: async (
+        configService: ConfigService,
+        tokenService: TokenService,
+
+      ) => {
         return {
           installSubscriptionHandlers:true,
           playground: true,
@@ -38,6 +42,18 @@ const pubsub = new RedisPubSub({
             'graphql-ws': true,
             'subscriptions-transport-ws': true,
           },
+          onConnect:(connectionParams) => {
+            const token = tokenService.extractToken(connectionParams);
+
+            if (!token) {
+              throw new Error('Token not provided');
+            }
+            const user = tokenService.validateToken(token);
+            if (!user) {
+              throw new Error('Invalid token');
+            }
+            return { user };
+          }
         };
       },
     }),
